fa1428f0802dde5f831ae5b4a75975f56da3fb77
DaoVinhLoc26 <jakkydao26@gmail.com>
2023-12-17 20:18:36 +0700

    AP

---
+++ FootballManagementSystem/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FootballManagementSystem
+{
+
+    internal class Player : APlayer
+    {
+        /*
+        public string Name;
+        public int Age;
+        public int attack;
+        public int defense;
+        public int stamina;
+        public int speed;
+        public int power;
+        */
+
+        public override void GetInfo()
+        {
+            //Console.WriteLine("Name |   Age |   Attack  |   Defense |   Stamina |   Speed   |   Power");
+            Console.WriteLine("{0}  |   {0} |   {0} |   {0} |   {0} |   {0} |   {0} |   {0}", Name, Age, Attack, Defense, Stamina, Speed, Power);
+            /*
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Attack: {0}", Attack);
+            Console.WriteLine("Defense: {0}", Defense);
+            Console.WriteLine("Stamina: {0}", Stamina);
+            Console.WriteLine("Speed: {0}", Speed);
+            Console.WriteLine("Power: {0}", Power);
+            */
+        }
+    }
+}
+
+
---
+++ FootballManagementSystem/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManagementSystem
+{
+   
+        class Program
+        {
+            private static bool found;
+
+            static void Menu()
+            {
+                Console.WriteLine("----------------------");
+                Console.WriteLine("1. Create new Player");
+                Console.WriteLine("2. List of Players");
+                Console.WriteLine("3. Search Player by name");
+                Console.WriteLine("4. Delete Player");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("----------------------");
+                Console.Write("Please choose an option: ");
+            }
+
+            static void Main(string[] args)
+            {
+                Club<APlayer> club = new Club<APlayer>();
+
+                int opt = 0;
+                while (opt != 5)
+                {
+                    Menu();
+                    try
+                    {
+                        opt = int.Parse(Console.ReadLine());
+                    
+                        switch (opt)
+                        {
+                            case 1:
+                                Player player = new Player();
+                                club.Add(player);
+                                break;
+                            case 2:
+                                Console.WriteLine("List of players:");
+                                if (club.PlayerList.Count == 0)
+                                {
+                                    Console.WriteLine("No players!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Name |   Age |   Attack  |   Defense |   Stamina |   Speed   |   Power");
+                                    foreach (APlayer player1 in club)
+                                    {
+                                        player1.GetInfo();
+                                    }
+                                }
+                                break;
+                            case 3:
+                                if (club.PlayerList.Count == 0)
+                                {
+                                    Console.WriteLine("No players!");
+                                }
+                                else
+                                {
+                                    Console.Write("Input player name you want to search: ");
+                                    string name = Console.ReadLine();
+                                    bool found = false;
+                                    foreach (APlayer player1 in club)
+                                    {
+                                        if (name.Equals(player1.Name))
+                                        {
+                                            player1.GetInfo();
+                                            found = true;
+                                        }
+                                    }
+                                    if (!found)
+                                    {
+                                        Console.WriteLine("The name {0} is not found!", name);
+                                    }
+                                }
+                                break;
+                            case 4:
+                                Console.Write("\nEnter player name to delete: ");
+                                string playername = Console.ReadLine();
+                                found = false;
+
+                                foreach (APlayer player1 in club)
+                                {
+                                    if (player1.Name == playername)
+                                    {
+                                        club.DeletePlayer(player1);
+                                        found = true;
+                                        break;
+                                    }
+                                }
+
+                                if (found)
+                                {
+                                    Console.WriteLine("\nPlayer deleted successfully.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nPlayer not found.");
+                                }
+
+                                break;
+                            case 5: return;
+                        }
+                    }
+                    catch (Exception ex) { Console.WriteLine("Wrong option!"); }
+            }
+            }
+        }
+    }
+
+
